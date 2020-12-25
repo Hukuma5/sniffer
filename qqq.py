3fae32e38c91916272827e89ed4362dbbc10d356
Hukuma5 <56114535+Hukuma5@users.noreply.github.com>
2020-12-25 20:47:29 +0300

    Update qqq.py

--- qqq.py
+++ qqq.py
@@ -2,16 +2,16 @@ result = []
 stroka = '' 
 
 with open(r'C:\Users\User\Downloads\pipec.txt', 'r') as inf: 
-file = inf.readlines() 
+  file = inf.readlines() 
 
 for i in file: 
-for j in range(len(i) - 2): 
-if i[j] == '\\' and i[j + 1] == 'n': 
-result.append(stroka) 
-stroka = '' 
-break 
-stroka += i[j] 
+  for j in range(len(i) - 2): 
+    if i[j] == '\\' and i[j + 1] == 'n': 
+      result.append(stroka) 
+      stroka = '' 
+      break 
+    stroka += i[j] 
 
 with open(r'C:\Users\User\Downloads\pipec.txt', 'w') as ouf: 
-for elem in result: 
-ouf.write(elem + '\n')
+  for elem in result: 
+    ouf.write(elem + '\n')
